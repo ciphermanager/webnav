2dd3d3de1126f168e4da308b0d5b99c5616d2ff1
二营长 <mr.pujing666@qq.com>
2020-06-22 15:55:00 +0800

    add data.js

---
+++ js/data.js
@@ -0,0 +1,33 @@
+var menus = {
+    "生活": {
+        "搜索": {
+            "id": "ss"
+        }, "娱乐": {
+            "id": "yule"
+        }
+    },
+    "学习": {
+        "数据库": {
+            "id": "shujuku"
+        }, "文科": {
+            "id": "wk"
+        }
+    },
+}
+// menus={
+// 菜单集合
+// menu:{
+// 第一大类
+//     submenu{
+// 第一大类的子类
+// id:idnum
+// 子类的id
+//     }
+// }}
+// data=[
+// {name:名称，url，路径，desc：简介，parentid：父id}，
+// ]
+var datas = [
+    { "name": "百度", "url": "http://www.baidu.com", "desc": "搜索引擎", "parentid": "ss" },
+{"name":"畅想之星光盘数据库","url":"http://cxstar.ntlib.org.cn/bookcd/index/index.do","desc":"速度尚可，免ip认证","parent":"shujuku"}
+]
